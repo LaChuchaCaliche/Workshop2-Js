8d048597a1021a54fe089c2728f1d6d003cfe8a6
LaChuchaCaliche <caduranb@unal.edu.co>
2024-04-22 20:19:15 -0500

    final wkshop2

--- Julian_Travel.js
+++ Julian_Travel.js
@@ -33,108 +33,175 @@
 //             else {break;}
 //             }
 // console.log(souvenirArray)
-souvenirQuantity = prompt(" Paquetes de souvenires : 1. 5 , 2.3 , 3. 1")
-function addSouvenirs(array,a,b,c){
+// souvenirQuantity = prompt(" Paquetes de souvenires : 1. 5 , 2.3 , 3. 1")
+// function addSouvenirs(array,a,b,c){
     
-    array.push(a)
-    console.log(typeof(a))
-    array.push(b)
-    console.log(typeof(b))
-    if (c == 1){
-        array.push(true)
-        console.log(typeof(true))
-    }
-    if (c == 2){
-        array.push(false)
-        console.log(typeof(false))
+//     array.push(a)
+//     console.log(typeof(a))
+//     array.push(b)
+//     console.log(typeof(b))
+//     if (c == 1){
+//         array.push(true)
+//         console.log(typeof(true))
+//     }
+//     if (c == 2){
+//         array.push(false)
+//         console.log(typeof(false))
 
-    }
+//     }
     
-    return (souvenirs.push(array))
-}
-let souvenirs = []
-let i = 5
-let f = 3
-let g = 1
-if (souvenirQuantity == 1) {
+//     return (souvenirs.push(array))
+// }
+// let souvenirs = []
+// let i = 5
+// let f = 3
+// let g = 1
+// if (souvenirQuantity == 1) {
     
-    if (i =!0){
-        let souvenirName = prompt("Ingrese nombre de souvenir ")
-        let souvenirPrice = Number(prompt(" Ingrese el precio del souvenir "))
-        let souvenirDisp = Number(prompt(" Ingrese disponibilidad 1. Disponible 2. No disponible"))
-        let souvenir1 = []
-        addSouvenirs(souvenir1,souvenirName,souvenirPrice,souvenirDisp)
-        i =-1
-        }
-        if (i =! 0 ){
-            let souvenirName = prompt("Ingrese nombre de souvenir ")
-            let souvenirPrice = Number(prompt(" Ingrese el precio del souvenir "))
-            let souvenirDisp = Number(prompt(" Ingrese disponibilidad 1. Disponible 2. No disponible"))
-            let souvenir2 = []
-            addSouvenirs(souvenir2,souvenirName,souvenirPrice,souvenirDisp)
-            i =-1
-        }
-            if (i =! 0 ){
-                let souvenirName = prompt("Ingrese nombre de souvenir ")
-                let souvenirPrice = Number(prompt(" Ingrese el precio del souvenir "))
-                let souvenirDisp = Number(prompt(" Ingrese disponibilidad 1. Disponible 2. No disponible"))
-                let souvenir3 = []
-                addSouvenirs(souvenir3,souvenirName,souvenirPrice,souvenirDisp)
-                i =-1
-                }
-                if (i =! 0 ){
-                    let souvenirName = prompt("Ingrese nombre de souvenir ")
-                    let souvenirPrice = Number(prompt(" Ingrese el precio del souvenir "))
-                    let souvenirDisp = Number(prompt(" Ingrese disponibilidad 1. Disponible 2. No disponible"))
-                    let souvenir4 = []
-                    addSouvenirs(souvenir4,souvenirName,souvenirPrice,souvenirDisp)
-                    i =-1
-                    }
-                    if (i =! 0 ){
-                        let souvenirName = prompt("Ingrese nombre de souvenir ")
-                        let souvenirPrice = Number(prompt(" Ingrese el precio del souvenir "))
-                        let souvenirDisp = Number(prompt(" Ingrese disponibilidad 1. Disponible 2. No disponible"))
-                        let souvenir5 = []
-                        addSouvenirs(souvenir5,souvenirName,souvenirPrice,souvenirDisp)
-                        i =-1
-                    }
-            }
+//     if (i =!0){
+//         let souvenirName = prompt("Ingrese nombre de souvenir ")
+//         let souvenirPrice = Number(prompt(" Ingrese el precio del souvenir "))
+//         let souvenirDisp = Number(prompt(" Ingrese disponibilidad 1. Disponible 2. No disponible"))
+//         let souvenir1 = []
+//         addSouvenirs(souvenir1,souvenirName,souvenirPrice,souvenirDisp)
+//         i =-1
+//         }
+//         if (i =! 0 ){
+//             let souvenirName = prompt("Ingrese nombre de souvenir ")
+//             let souvenirPrice = Number(prompt(" Ingrese el precio del souvenir "))
+//             let souvenirDisp = Number(prompt(" Ingrese disponibilidad 1. Disponible 2. No disponible"))
+//             let souvenir2 = []
+//             addSouvenirs(souvenir2,souvenirName,souvenirPrice,souvenirDisp)
+//             i =-1
+//         }
+//             if (i =! 0 ){
+//                 let souvenirName = prompt("Ingrese nombre de souvenir ")
+//                 let souvenirPrice = Number(prompt(" Ingrese el precio del souvenir "))
+//                 let souvenirDisp = Number(prompt(" Ingrese disponibilidad 1. Disponible 2. No disponible"))
+//                 let souvenir3 = []
+//                 addSouvenirs(souvenir3,souvenirName,souvenirPrice,souvenirDisp)
+//                 i =-1
+//                 }
+//                 if (i =! 0 ){
+//                     let souvenirName = prompt("Ingrese nombre de souvenir ")
+//                     let souvenirPrice = Number(prompt(" Ingrese el precio del souvenir "))
+//                     let souvenirDisp = Number(prompt(" Ingrese disponibilidad 1. Disponible 2. No disponible"))
+//                     let souvenir4 = []
+//                     addSouvenirs(souvenir4,souvenirName,souvenirPrice,souvenirDisp)
+//                     i =-1
+//                     }
+//                     if (i =! 0 ){
+//                         let souvenirName = prompt("Ingrese nombre de souvenir ")
+//                         let souvenirPrice = Number(prompt(" Ingrese el precio del souvenir "))
+//                         let souvenirDisp = Number(prompt(" Ingrese disponibilidad 1. Disponible 2. No disponible"))
+//                         let souvenir5 = []
+//                         addSouvenirs(souvenir5,souvenirName,souvenirPrice,souvenirDisp)
+//                         i =-1
+//                     }
+//             }
     
-if (souvenirQuantity == 2) {
-    if (f =! 0 ){
-        let souvenirName = prompt("Ingrese nombre de souvenir ")
-        let souvenirPrice = Number(prompt(" Ingrese el precio del souvenir "))
-        let souvenirDisp = Number(prompt(" Ingrese disponibilidad 1. Disponible 2. No disponible")) 
-        let souvenir1 = []
-        addSouvenirs(souvenir1,souvenirName,souvenirPrice,souvenirDisp)
-        f =-1
-    }
-        if (f =! 0 ){
-            let souvenirName = prompt("Ingrese nombre de souvenir ")
-            let souvenirPrice = Number(prompt(" Ingrese el precio del souvenir "))
-            let souvenirDisp = Number(prompt(" Ingrese disponibilidad 1. Disponible 2. No disponible")) 
-            let souvenir2 = []
-            addSouvenirs(souvenir2,souvenirName,souvenirPrice,souvenirDisp)
-            f =-1
-        }
-            if (f =! 0 ){
-                let souvenirName = prompt("Ingrese nombre de souvenir ")
-                let souvenirPrice = Number(prompt(" Ingrese el precio del souvenir "))
-                let souvenirDisp = Number(prompt(" Ingrese disponibilidad 1. Disponible 2. No disponible"))
-                let souvenir3 = []
-                addSouvenirs(souvenir3,souvenirName,souvenirPrice,souvenirDisp)
-                f =-1
-            }
-}
-if (souvenirQuantity == 3){
-    if (g =! 0 ){
-        let souvenirName = prompt("Ingrese nombre de souvenir ")
-        let souvenirPrice = Number(prompt(" Ingrese el precio del souvenir "))
-        let souvenirDisp = Number(prompt(" Ingrese disponibilidad 1. Disponible 2. No disponible"))
-        let souvenir1 = []
-        addSouvenirs(souvenir1,souvenirName,souvenirPrice,souvenirDisp)
-        g =-1}
+// if (souvenirQuantity == 2) {
+//     if (f =! 0 ){
+//         let souvenirName = prompt("Ingrese nombre de souvenir ")
+//         let souvenirPrice = Number(prompt(" Ingrese el precio del souvenir "))
+//         let souvenirDisp = Number(prompt(" Ingrese disponibilidad 1. Disponible 2. No disponible")) 
+//         let souvenir1 = []
+//         addSouvenirs(souvenir1,souvenirName,souvenirPrice,souvenirDisp)
+//         f =-1
+//     }
+//         if (f =! 0 ){
+//             let souvenirName = prompt("Ingrese nombre de souvenir ")
+//             let souvenirPrice = Number(prompt(" Ingrese el precio del souvenir "))
+//             let souvenirDisp = Number(prompt(" Ingrese disponibilidad 1. Disponible 2. No disponible")) 
+//             let souvenir2 = []
+//             addSouvenirs(souvenir2,souvenirName,souvenirPrice,souvenirDisp)
+//             f =-1
+//         }
+//             if (f =! 0 ){
+//                 let souvenirName = prompt("Ingrese nombre de souvenir ")
+//                 let souvenirPrice = Number(prompt(" Ingrese el precio del souvenir "))
+//                 let souvenirDisp = Number(prompt(" Ingrese disponibilidad 1. Disponible 2. No disponible"))
+//                 let souvenir3 = []
+//                 addSouvenirs(souvenir3,souvenirName,souvenirPrice,souvenirDisp)
+//                 f =-1
+//             }
+// }
+// if (souvenirQuantity == 3){
+//     if (g =! 0 ){
+//         let souvenirName = prompt("Ingrese nombre de souvenir ")
+//         let souvenirPrice = Number(prompt(" Ingrese el precio del souvenir "))
+//         let souvenirDisp = Number(prompt(" Ingrese disponibilidad 1. Disponible 2. No disponible"))
+//         let souvenir1 = []
+//         addSouvenirs(souvenir1,souvenirName,souvenirPrice,souvenirDisp)
+//         g =-1}
 
-}
-document.write(souvenirs)
-console.log(souvenirs)
+// }
+// document.write(souvenirs)
+// console.log(souvenirs)
+
+//  let travelCost = Number(prompt("Ingrese su presupuesto inicial"))
+//  let travelFood = Number(prompt("Ingrese el estimado de gastos en comida "))
+//  let travelInstance = Number(prompt("Ingrese el estimado de alojamiento "))
+//  let travelingCosts = Number(prompt("Ingrese el costo de desplazamientos "))
+//  let spare = travelCost - (travelFood+travelInstance+travelingCosts)
+//  console.log(" Tu presupuesto para adquirir un souvenir es de : "+spare)
+//  const article = 300
+//  const suggestion = spare> article ? "puedes adquirir el articulo" : "no puedes adquirir el articulo"
+//  console.log(suggestion)
+//  let souvenirs = []
+//  while(true){
+//      let menu = Number(prompt((" 1. Desea agregar un souvenir " + " 2. Desea saber que souvenir es mas economico " + " 3. Desea saber que souvenir es mas costoso"+ " 4. Desea ver los souvenirs agregados" +" 5. Desea salir")))
+
+//       if(isNaN(menu) || 1>menu || menu>5){
+//           alert("Ingresaste de forma incorrecta")
+//           }
+//      if (menu === 1 ){
+//          let souvenir = {
+//              name : prompt("Ingrese el nombre del souvenir"),
+//              value: Number(prompt("Ingrese el precio del souvenir"))
+//          }
+//          console.log(souvenir)
+//          souvenirs.push(souvenir)
+//      }
+//      else if (menu === 2 ){
+//              let cheapSouvenir = souvenirs.reduce((anterior,siguiente)=>{return(anterior.value<siguiente.value)? anterior : siguiente
+//              })
+//          console.log(cheapSouvenir)}
+//      else if (menu === 3){
+//          let expensiveSouvenir = souvenirs.reduce((anterior,siguiente)=>{return(anterior.value>siguiente.value)? anterior: siguiente
+//          })
+//          console.log(expensiveSouvenir)
+//          }
+//      else if(menu ===4 ){
+//          console.log(souvenirs.forEach((element)=>console.log(element)))
+//      }
+//      else if (menu===5){
+//          alert("Gracias por usar la aplicacion")
+//          break}
+
+//      }    
+// let presupuestoInicial = Number(prompt("Ingrese su presupuesto en gastos"))
+// let presupuestoComida = Number(prompt("Ingrese el precio del restaurante que desea visitar"))
+// let presupuestoLibros = Number(prompt("Ingrese el costo del libro"))
+// let saves = presupuestoInicial*0.10
+// let combo1 = presupuestoInicial-(saves+presupuestoComida)
+// let combo2 = presupuestoInicial-(saves+presupuestoLibros)
+// let combo3 = presupuestoInicial-(saves+presupuestoLibros+presupuestoComida)
+// while(true){
+//     if (combo3 >=0){ 
+//         console.log("Tu presupuesto de hoy te permite comer y comprar el libro esto es tu sobrante "+com)
+//         break
+//     }
+//     else if(combo1 >= 0){
+//         console.log("Puedes comer fuera hoy, esto es tu sobrante  " + combo1)
+//         break
+//     }
+//     else if (combo2 >=0){
+//         console.log("Puedes comprar el libro que quieres,esto es tu sobrante " + combo2)
+//         break
+//     }    
+//     else{
+//         console.log("Tu presupuesto no te alcanza para nada, POBRE")
+//         break
+//     }
+//     }
